6593c93192441813ca8c51735676b6b0a670b8d2
Jerico Dcosta <68363466+Jerico007@users.noreply.github.com>
2023-07-17 10:09:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ function threeSum(arr, target) {
 
         let closestSum = arr[0] + arr[1] + arr[2];
 
-    
-
         //Start from i = 1 cause first three sums are already taken care of
         for (let i = 1; i < arr.length - 2; i++) {
           let j = i + 1;
@@ -17,7 +15,7 @@ function threeSum(arr, target) {
           while (j < k) {
             let sum = arr[i] + arr[j] + arr[k];
 
-            if (Math.abs(closestSum - target) > Math.abs(sum - target)) {
+            if ((closestSum - target) > (sum - target)) {
               closestSum = sum;
             }
 
